319c2064d560fda5111d2c10f785b7cecb2d714d
Pablo Tesone <tesonep@gmail.com>
2023-01-04 18:20:21 +0100

    Adding bench for the startup of the image

---
+++ benchs/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGES="Pharo10 Pharo11"
+VMs="latest latest10"
+PHARO_CMD="eval 1+1"
+
+source "$1/bench.inc"
+
+IMAGES="Pharo11ComposedImage"
+VMs="newImageFormat"
+
+source "$1/bench.inc"
