329648a4bec1771919b49808efa07be59dc9f79d
Pablo Tesone <tesonep@gmail.com>
2023-01-04 20:20:31 +0100

    Adding benchs

---
+++ benchs/smarkCompiler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGES="Pharo11SMark"
+VMs="latest9 latest10"
+PHARO_CMD="eval SMarkCompiler run: 100"
+
+source "$1/bench.inc"
+
+IMAGES="Pharo11ComposedImageSMark"
+VMs="newImageFormat"
+
+source "$1/bench.inc"
