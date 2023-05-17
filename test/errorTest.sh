8dd774e12ebde8855ff760251e581358c0a023f1
Durieux Pol <pol.durieux.etu@univ-lille.fr>
2023-05-17 11:39:26 +0200

    added comments and another test

--- test/errorTest.sh
+++ test/errorTest.sh
@@ -2,7 +2,7 @@
 
 # Before any test, this command is executed
 oneTimeSetUp() {
-    ITERATIONS=1 BENCHES_SCRIPT_DIR=test/testError ./runAll.sh aDate
+    BENCHES_SCRIPT_DIR=test/testError ./runAll.sh aDate
 }
 
 # After all tests, the results files are removed
@@ -10,25 +10,33 @@ oneTimeTearDown() {
     rm -rf ./_build/results
 }
 
-# Test if the line in the csv file starts with "OK"
+# Checks that the first line in the csv file starts with "ERROR"
 testLine1IsError() {
     local line=$(tail -4 ./_build/results/benchmarkWithError-Pharo11-latest10/benchmarkWithError-Pharo11-latest10-aDate.csv | head -1)
     assertTrue '[[ "$line" == ERROR* ]]'
 }
 
+# Checks that the second line in the csv file starts with "OK"
 testLine2IsOk() {
     local line=$(tail -3 ./_build/results/benchmarkWithError-Pharo11-latest10/benchmarkWithError-Pharo11-latest10-aDate.csv | head -1)
     assertTrue '[[ "$line" == OK* ]]'
 }
 
+# Checks that the third line in the csv file starts with "ERROR"
 testLine3IsError() {
     local line=$(tail -2 ./_build/results/benchmarkWithError-Pharo11-latest10/benchmarkWithError-Pharo11-latest10-aDate.csv | head -1)
     assertTrue '[[ "$line" == ERROR* ]]'
 }
 
+# Checks that the fourth line in the csv file starts with "OK"
 testLine4IsOk() {
     local line=$(tail -1 ./_build/results/benchmarkWithError-Pharo11-latest10/benchmarkWithError-Pharo11-latest10-aDate.csv)
     assertTrue '[[ "$line" == OK* ]]'
+}
+
+# Checks that there are only 4 lines in the csv file
+testOnlyFourLinesInFile() {
+    assertEquals 4 "$(cat ./_build/results/benchmarkWithError-Pharo11-latest10/benchmarkWithError-Pharo11-latest10-aDate.csv | wc -l)"
 }
 
 # Load shUnit2.
--- test/testError/benchmarkWithError.sh
+++ test/testError/benchmarkWithError.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# This benchmark is used in errorTest.sh.
+
 IMAGES="Pharo11"
 VMs="latest10"
 ITERATIONS=4
+
+# This command only fails if ITERATION is odd, so that with 4 iterations, it fails one in two times.
+# In errorTest it is checked that the iterations indeed failed one in two times
 PHARO_CMD='eval ($ITERATION \\ 2) == 0 ifFalse: [ self error ]'
 
 runBenchs
