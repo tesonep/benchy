d14b55c3d3d5cbb87cdd821e0448d13dfe969c22
Hernán Morales Durand <hernan.morales@gmail.com>
2023-06-05 15:24:01 +0200

    Check if download directory exists instead of image file name

--- downloadImages.sh
+++ downloadImages.sh
@@ -22,12 +22,13 @@ IMAGES_NAMES=$(find $IMAGES_SCRIPT_DIR -iname "*.sh" | xargs -n1 bash -c 'basena
 
 # Lazy downloading of images, if image already exists, do not download again
 download_pharo_images () {
+	printf "Downloading pharo images\n"
 	for image in $IMAGES_NAMES; do
-		if [ ! -f "$image" ]; then
+		if [ ! -d "$BUILD_IMAGES_DIR/$image" ]; then
 			printf "Downloading image %s\n" $image
 			bash $IMAGES_SCRIPT_DIR/$image.sh $IMAGES_SCRIPT_DIR $BUILD_IMAGES_DIR
 		else
-			printf "Image already downloaded\n"
+			printf "Image already downloaded %s\n" $image
 		fi
 	done
 }
